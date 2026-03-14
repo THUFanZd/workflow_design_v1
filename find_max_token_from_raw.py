f5673f7a18e97ee629495af480e9c90d466ef276
peco <SatoruLi@163.com>
2026-03-14 13:41:08 +0800

    Delete find max raw file.

--- find_max_token_from_raw.py
+++
@@ -1,8 +0,0 @@
-import json
-# 打开feature_raw.json文件
-with open('feature_raw.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-first_activation_sentence_dict = data["activations"][0]
-max_token = first_activation_sentence_dict["tokens"][first_activation_sentence_dict["maxValueTokenIndex"]]
-print(max_token)
