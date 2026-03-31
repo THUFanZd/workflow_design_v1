f6914b3f11a0eefa11529df8bd00de4ef19089f4
peco <SatoruLi@163.com>
2026-03-31 19:05:41 +0800

    fix: remove an ignored file.

--- support_info/llm_api_info.py
+++
@@ -1,3 +0,0 @@
-api_key_file = "C:\\Users\\lzx\\Desktop\\\u7814\u4e00\u4e0b\\keys\\ali_api_key.txt"
-base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1"
-model_name = "qwen-plus"  # used in code "client.chat.completions.create" and OpenAIChatCompletionsModel's parameter "model"
